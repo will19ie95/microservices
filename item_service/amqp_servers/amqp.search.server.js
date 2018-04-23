5158eecf811058823f56e1b6ed6aeb3f93ad970b
Yong Lei <will19ie95@gmail.com>
2018-04-22 22:52:22 -0400

    tookout amqp search function

--- item_service/amqp_servers/amqp.search.server.js
+++ item_service/amqp_servers/amqp.search.server.js
@@ -10,110 +10,69 @@ amqp.connect('amqp://yong:yong@130.245.168.55', function (err, conn) {
   console.log(" [x] Item Server Connected to rabbitmq...")
   // Add Item 
   conn.createChannel(function (err, ch) {
-    var q = 'searchitem_rpc_queue';
-
     console.log(' [x] Awaiting RPC requests');
+    
+    var q = 'searchitem_rpc_queue';
     ch.assertQueue(q, { durable: true });
     ch.prefetch(1);
     ch.consume(q, function reply(search) {
-      var search_json = JSON.parse(search.content);
-      console.log(search_json)
-      var options = search_json.options;
-      var limit = options.limit;
-      var query = search_json.query;
-      var reply;
+      var reply = search(search);
+      ch.sendToQueue(search.properties.replyTo,
+        new Buffer(JSON.stringify(reply)),
+        { correlationId: search.properties.correlationId },
+        { persistent: true });
+      ch.ack(search);
+    });
+  });
+});
 
-      // consume the message DO THE WORK.
-      // 
-      console.log(" [.] searching(%s)", options.query_string);
+function search(search) {
 
-      // if true, return post by jwt user following
-      if (options.only_following) {
-        // find following for jwt user
-        User.findOne({ username: options.username }, function (err, user) {
+  var search_json = JSON.parse(search.content);
+  var options = search_json.options;
+  var limit = options.limit;
+  var query = search_json.query;
+  var reply;
+
+  // consume the message DO THE WORK.
+  // 
+  console.log(" [.] searching(%s)", options.query_string);
+
+  // if true, return post by jwt user following
+  if (options.only_following) {
+    // find following for jwt user
+    User.findOne({ username: options.username }, function (err, user) {
+      if (err) {
+        reply = {
+          status: "error",
+          message: "Error: " + err.stack
+        }
+      } else if (!user) {
+        reply = {
+          status: "error",
+          message: "User Not Found"
+        }
+      } else {
+        // list of following, only return if match any of these
+        var following = user.following;
+
+        // append username constraint if exist
+        if (options.username_filter) {
+          // possible duplication, fix me
+          following.push(options.username_filter)
+        }
+
+        var following_filter = { $in: following }
+
+        query["username"] = following_filter
+
+        Item.find(query, function (err, items) {
           if (err) {
             reply = {
               status: "error",
               message: "Error: " + err.stack
             }
-            ch.sendToQueue(search.properties.replyTo,
-              new Buffer(JSON.stringify(reply)),
-              { correlationId: search.properties.correlationId },
-              { persistent: true });
-            ch.ack(search);
-          }
-          if (!user) {
-            reply = {
-              status: "error",
-              message: "User Not Found"
-            }
-            ch.sendToQueue(search.properties.replyTo,
-              new Buffer(JSON.stringify(reply)),
-              { correlationId: search.properties.correlationId },
-              { persistent: true });
-            ch.ack(search);
-          } else {
-            // list of following, only return if match any of these
-            var following = user.following;
-
-            // append username constraint if exist
-            if (options.username_filter) {
-              // possible duplication, fix me
-              following.push(options.username_filter)
-            }
-
-            var following_filter = { $in: following }
-
-            query["username"] = following_filter
-
-            Item.find(query, function (err, items) {
-              if (err) {
-                reply = {
-                  status: "error",
-                  message: "Error: " + err.stack
-                }
-              }
-              if (!items) {
-                reply = {
-                  status: "error",
-                  message: "Item Not Found"
-                }
-              } else {
-                reply = {
-                  status: "OK",
-                  message: "Found Items",
-                  items: items.slice(0, limit)
-                }
-              }
-              ch.sendToQueue(search.properties.replyTo,
-                new Buffer(JSON.stringify(reply)),
-                { correlationId: search.properties.correlationId },
-                { persistent: true });
-              ch.ack(search);
-            })
-          }
-          
-        })
-      } else {
-        // append username constraint if exist
-        if (options.username_filter) {
-          // Create username key in query object
-          query["username"] = {
-            // append username_filter to search for it
-            $in: [
-              options.username_filter
-            ]
-          }
-        }
-        // only following is false, return all
-        Item.find(query, function (err, items) {
-          if (err) { 
-            reply = {
-              status: "error",
-              message: "Error: " + err.stack
-            }
-          }
-          if (!items) { 
+          } else if (!items) {
             reply = {
               status: "error",
               message: "Item Not Found"
@@ -125,15 +84,41 @@ amqp.connect('amqp://yong:yong@130.245.168.55', function (err, conn) {
               items: items.slice(0, limit)
             }
           }
-
-          ch.sendToQueue(search.properties.replyTo,
-            new Buffer(JSON.stringify(reply)),
-            { correlationId: search.properties.correlationId },
-            { persistent: true });
-          ch.ack(search);
+          return reply
         })
       }
-
-    });
-  });
-});
+    })
+  } else {
+    // append username constraint if exist
+    if (options.username_filter) {
+      // Create username key in query object
+      query["username"] = {
+        // append username_filter to search for it
+        $in: [
+          options.username_filter
+        ]
+      }
+    }
+    // only following is false, return all
+    Item.find(query, function (err, items) {
+      if (err) {
+        reply = {
+          status: "error",
+          message: "Error: " + err.stack
+        }
+      } else if (!items) {
+        reply = {
+          status: "error",
+          message: "Item Not Found"
+        }
+      } else {
+        reply = {
+          status: "OK",
+          message: "Found Items",
+          items: items.slice(0, limit)
+        }
+      }
+      return reply
+    })
+  }
+}
