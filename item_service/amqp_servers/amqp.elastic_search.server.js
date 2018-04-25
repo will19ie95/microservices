691a00239029daea6fdfae1bcbf34b504398f1f1
Yong Lei <will19ie95@gmail.com>
2018-04-24 20:39:45 -0400

    messed up

--- item_service/amqp_servers/amqp.elastic_search.server.js
+++ item_service/amqp_servers/amqp.elastic_search.server.js
@@ -28,7 +28,7 @@ amqp.connect('amqp://yong:yong@130.245.168.55', function (err, conn) {
       var options = search_json.options;
       var query_string = options.query_string;
       var limit = options.limit;
-      var query = search_json.query; query_string
+      var query = search_json.query;
       var reply;
 
       // consume the message DO THE WORK.
