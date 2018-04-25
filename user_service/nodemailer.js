8c7a915df1436b336dc6b84bf4cc0b56fa9c0ab8
Yong Lei <will19ie95@gmail.com>
2018-04-25 10:20:04 -0400

    added stmp

--- user_service/nodemailer.js
+++ user_service/nodemailer.js
@@ -1,17 +1,15 @@
 const nodemailer = require("nodemailer");
 const moment = require("moment")
-const postfix = require("./postfix") 
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  auth: {
-    user: "twitterclonezzz",
-    pass: "will19ie95"
+  host: 'localhost',
+  port: 25,
+  tls: {
+    rejectUnauthorized: false
   }
 });
 
-const sendMail = function(email, vToken) {
+const sendMail = function (email, vToken) {
   // setup e-mail data with unicode symbols
   var mailOptions = {
     from: 'Twitter Clone👻 <no.reply.twitterClone@gmail.com>', // sender address
@@ -19,14 +17,6 @@ const sendMail = function(email, vToken) {
     subject: 'Hello From Twitter Clone 👻', // Subject line
     text: "validation key: <" + vToken + ">", // plaintext body
   };
-
-  // send mail with postfix
-  // postfix.postfixSend(mailOptions, function (error, info) {
-  //   if (error) {
-  //     return console.log(error);
-  //   }
-  //   console.log('Message sent: ' + info.response);
-  // })
 
   // send mail with defined transport object
   transporter.sendMail(mailOptions, function (error, info) {
